a12fadc71a599c2121a60868535dc9b2fd32576b
DoNamHai07 <dnamhai07@gmail.com>
2026-07-02 08:41:59 +0700

    Add files via upload

--- CSDL/Namhai.sql
+++ CSDL/Namhai.sql
@@ -1,11 +1,1 @@
-CREATE TABLE Employees (
-    EmployeeID INT PRIMARY KEY,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    HireDate DATE
-);
-
-INSERT INTO Employees (EmployeeID, FirstName, LastName, HireDate)
-VALUES (1, 'John', 'Doe', '2024-01-15');
-
-SELECT * FROM Employees;
+Create database CSDL_thuchanh;
